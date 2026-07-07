8be17118fc1ad7b4c064a6327f9db27e18ac407a
durgeshkhatai06 <nandandurgesh27@gmail.com>
2026-07-07 18:46:35 +0530

    code added

--- Multithreading and Multiprocessing/factorial_multi_processing.py
+++ Multithreading and Multiprocessing/factorial_multi_processing.py
@@ -23,3 +23,17 @@ def computer_factorial(number):
     result=math.factorial(number)
     print(f"Factorial of {number} is {result}")
     return result
+
+if __name__=="__main__":
+    numbers=[5000,6000,700,8000]
+
+    start_time=time.time()
+
+    ##create a pool of worker processes
+    with multiprocessing.Pool() as pool:
+        results=pool.map(computer_factorial,numbers)
+
+    end_time=time.time()
+
+    print(f"Results: {results}")
+    print(f"Time taken: {end_time - start_time} seconds")
