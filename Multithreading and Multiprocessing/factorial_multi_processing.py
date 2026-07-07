3210bd6a95745a8f8b41a94700cbe871d40c2a6d
durgeshkhatai06 <nandandurgesh27@gmail.com>
2026-07-07 18:38:08 +0530

    code added

---
+++ Multithreading and Multiprocessing/factorial_multi_processing.py
@@ -0,0 +1,25 @@
+'''
+Real-World Example: Multiprocessing for CPU-bound Tasks
+Scenario: Factorial Calculation
+Factorial calculations, especially for large numbers, 
+involve significant computational work. Multiprocessing 
+can be used to distribute the workload across multiple 
+CPU cores, improving performance.
+
+'''
+
+import multiprocessing
+import math
+import sys
+import time
+
+# Increase the maximum number of digits for integer conversion
+sys.set_int_max_str_digits(100000)
+
+## function to compute factorials of a given number 
+
+def computer_factorial(number):
+    print(f"Computing factorial of {number}")
+    result=math.factorial(number)
+    print(f"Factorial of {number} is {result}")
+    return result
